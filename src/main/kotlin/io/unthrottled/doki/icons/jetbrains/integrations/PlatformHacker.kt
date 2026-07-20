3ba43948d7828507453f6d0ca1027ca435b1ef25
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2026-07-20 17:05:57 -0500

    fix: main & side toolwindows action buttons not theming correctly.
    - Brings back javassist dependency

---
+++ src/main/kotlin/io/unthrottled/doki/icons/jetbrains/integrations/PlatformHacker.kt
@@ -0,0 +1,93 @@
+package io.unthrottled.doki.icons.jetbrains.integrations
+
+import io.unthrottled.doki.icons.jetbrains.path.IconPathReplacementComponent
+import io.unthrottled.doki.icons.jetbrains.tools.Logging
+import io.unthrottled.doki.icons.jetbrains.tools.logger
+import io.unthrottled.doki.icons.jetbrains.tools.runSafely
+import javassist.ClassClassPath
+import javassist.ClassPool
+import javassist.expr.ExprEditor
+import javassist.expr.MethodCall
+
+object PlatformHacker : Logging {
+  init {
+    IconPathReplacementComponent.installComponents()
+    hackPlatform()
+  }
+
+  private fun hackPlatform() {
+    fixEXPUIButton()
+    fixEXPUIDefaultButton()
+    fixEXPUIRunWidget()
+    fixEXPUIStopButton()
+  }
+
+  private fun fixEXPUIStopButton() = fixEXPUI(
+    "com.intellij.execution.ui.RunState",
+    "com.intellij.execution.ui.StopWithDropDownAction",
+    "update",
+    "fixStopButton"
+  )
+
+  private fun fixEXPUIRunWidget() {
+    val fixName = "fixRunWidget"
+    val uiFQ = "com.intellij.execution.ui.RunState"
+    fixEXPUI(
+      uiFQ,
+      "com.intellij.execution.ui.RunWidgetButtonLook",
+      "paintIcon",
+      fixName
+    )
+    fixEXPUI(
+      uiFQ,
+      "com.intellij.execution.ui.RedesignedRunConfigurationSelector",
+      "update",
+      fixName
+    )
+  }
+
+  private fun fixEXPUIButton() = fixEXPUI(
+    "com.intellij.openapi.wm.impl.SideStack",
+    "com.intellij.openapi.wm.impl.SquareStripeButtonLook",
+    "paintIcon",
+    "fixButton"
+  )
+
+  private fun fixEXPUIDefaultButton() = fixEXPUI(
+    "com.intellij.ide.ui.laf.darcula.ui.DarculaMenuSeparatorUI",
+    "com.intellij.ide.ui.laf.darcula.ui.DarculaOptionButtonUI",
+    "paintArrow",
+    "fixDefaultButton"
+  )
+
+  private fun fixEXPUI(
+    uiFQ: String,
+    uiChildFQ: String,
+    iconMethod: String,
+    fixName: String = ::fixEXPUI.name
+  ) {
+    runSafely({
+      val cp = ClassPool(true)
+      cp.insertClassPath(
+        ClassClassPath(
+          Class.forName(uiFQ),
+        ),
+      )
+      val ctClass = cp[uiChildFQ]
+      ctClass.getDeclaredMethods(iconMethod).forEach { doPaintText ->
+        doPaintText.instrument(
+          object : ExprEditor() {
+            override fun edit(m: MethodCall?) {
+              if (m?.methodName == "toStrokeIcon") {
+                m.replace($$"{ $_ = $1; }")
+              }
+            }
+          },
+        )
+      }
+      ctClass.toClass()
+    }) {
+      logger().warn("Unable to hack '$fixName' for raisins", it)
+    }
+  }
+}
--- src/main/kotlin/io/unthrottled/doki/icons/jetbrains/listeners/ApplicationLifecycleListener.kt
+++ src/main/kotlin/io/unthrottled/doki/icons/jetbrains/listeners/ApplicationLifecycleListener.kt
@@ -3,8 +3,14 @@ package io.unthrottled.doki.icons.jetbrains.listeners
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.project.DumbAware
 import io.unthrottled.doki.icons.jetbrains.PluginMaster
+import io.unthrottled.doki.icons.jetbrains.integrations.PlatformHacker
 
 class ApplicationLifecycleListener : AppLifecycleListener, DumbAware {
+  companion object {
+    init {
+      PlatformHacker.toString()
+    }
+  }
   override fun appClosing() {
     PluginMaster.getInstance().dispose()
   }
